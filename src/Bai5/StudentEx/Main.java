d15f46f9e87f8d2d26bd974a7a44d0ff0bda5165
tnd18091995 <tnd18091995@gmail.com>
2024-03-25 14:59:46 +0700

    b5studentdone

---
+++ src/Bai5/StudentEx/Main.java
@@ -0,0 +1,8 @@
+package Bai5.StudentEx;
+
+public class Main {
+    public static void main(String[] args) {
+        Student S1 = new Student("Jin","C1");
+        Student S2 = new Student("Tim","C12");
+    }
+}
---
+++ src/Bai5/StudentEx/Student.java
@@ -0,0 +1,30 @@
+package Bai5.StudentEx;
+
+public class Student {
+    private String name;
+    private String classes;
+
+    public Student() {
+    }
+
+    public Student(String name, String classes) {
+        this.name = name;
+        this.classes = classes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
